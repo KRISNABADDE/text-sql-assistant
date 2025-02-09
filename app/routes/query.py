2262e792952aeca0c2835d9ec8feac45625a993b
KRISNABADDE <krisnabadde@gamil.com>
2025-02-09 18:00:39 +0530

    bug fixed

--- app/routes/query.py
+++ app/routes/query.py
@@ -9,6 +9,6 @@ router = APIRouter()
 
 @router.post("/query", response_class=StreamingResponse)
 async def get_sql(query: QueryRequest):
-    if not query.natural_query:
+    if not query.natural_query.strip():
         return StreamingResponse('PLEASE INTER QUERY')
     return StreamingResponse(convert_to_sql(query.natural_query), media_type="text/plain")
